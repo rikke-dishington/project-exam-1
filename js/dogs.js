eee45951c03c4be73f6f3b6ba3f9c718e983490c
rikke-dishington <85433495+rikke-dishington@users.noreply.github.com>
2023-01-15 19:27:22 +0100

    Startup
    * Created all folders and files.
    * Uploaded all images.
    * Started on html and css for home page.

---
+++ js/dogs.js
@@ -0,0 +1,26 @@
+[
+  {
+    image: "/images/crest_hunting.png",
+    name: "Fire Crest",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam condimentum justo tincidunt, fermentum nisl non, varius nisi. Integer vulputate lectus ut elit bibendum finibus. Sed laoreet aliquet lorem dapibus eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan tempor arcu, in iaculis enim lacinia nec. Etiam efficitur finibus nulla, ac placerat nisl aliquet eget.",
+  },
+  {
+    image: "/images/pippin_field_trial.png",
+    name: "Pippin",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam condimentum justo tincidunt, fermentum nisl non, varius nisi. Integer vulputate lectus ut elit bibendum finibus. Sed laoreet aliquet lorem dapibus eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan tempor arcu, in iaculis enim lacinia nec. Etiam efficitur finibus nulla, ac placerat nisl aliquet eget.",
+  },
+  {
+    image: "/images/sage_goose.png",
+    name: "Sage",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam condimentum justo tincidunt, fermentum nisl non, varius nisi. Integer vulputate lectus ut elit bibendum finibus. Sed laoreet aliquet lorem dapibus eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan tempor arcu, in iaculis enim lacinia nec. Etiam efficitur finibus nulla, ac placerat nisl aliquet eget.",
+  },
+  {
+    image: "/images/marco_water_apport.png",
+    name: "Marco",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam condimentum justo tincidunt, fermentum nisl non, varius nisi. Integer vulputate lectus ut elit bibendum finibus. Sed laoreet aliquet lorem dapibus eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan tempor arcu, in iaculis enim lacinia nec. Etiam efficitur finibus nulla, ac placerat nisl aliquet eget.",
+  },
+];
