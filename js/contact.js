5bc482e4b6e5c7f9261693471f2167043f8a3966
rikke-dishington <85433495+rikke-dishington@users.noreply.github.com>
2023-01-21 22:23:13 +0100

    Contact form validation

---
+++ js/contact.js
@@ -0,0 +1,53 @@
+const form = document.querySelector("#contactForm");
+const fullName = document.querySelector("#fullName");
+const fullNameError = document.querySelector("#fullNameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
+const message = document.querySelector("#message");
+const messageError = document.querySelector("#messageError");
+
+function submitForm(event) {
+  event.preventDefault();
+
+  if (checkLength(fullName.value, 5) === true) {
+    fullNameError.style.display = "none";
+  } else {
+    fullNameError.style.display = "block";
+  }
+
+  if (validateEmail(email.value) === true) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+
+  if (checkLength(subject.value, 15) === true) {
+    subjectError.style.display = "none";
+  } else {
+    subjectError.style.display = "block";
+  }
+
+  if (checkLength(message.value, 25) === true) {
+    messageError.style.display = "none";
+  } else {
+    messageError.style.display = "block";
+  }
+}
+
+form.addEventListener("submit", submitForm);
+
+function checkLength(value, len) {
+  if (value.trim().length > len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
