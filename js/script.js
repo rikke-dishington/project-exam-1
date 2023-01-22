1d77feea1832e8c5faaff71055687fac833bded4
rikke-dishington <85433495+rikke-dishington@users.noreply.github.com>
2023-01-22 16:38:04 +0100

    API update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const resultsContainer = document.querySelector(".blog");
 
-const url =
+const baseURL =
   "https://rikkedishingtonschool.com/project-exam/wp-json/wp/v2/posts";
 
 async function fetchPosts() {
   try {
-    const response = await fetch(url);
+    const response = await fetch(baseURL);
     const json = await response.json();
 
     console.log(json);
@@ -16,13 +16,13 @@ async function fetchPosts() {
       resultsContainer.innerHTML += `<a href="blog-post.html?id=${post.id}" class="card">
                                                 <div class="image" style="background-image: url(${post.featured_media.source_url});"></div>
                                                 <div class="details">
-                                                    <h2 class="name">${post.title.rendered}</h2>                                                                                                                                                        
+                                                  <h2 class="name">${post.title.rendered}</h2>                                                                                                                                                        
                                                 </div>
                                             </a>`;
     });
   } catch (error) {
     console.log(error);
-    resultsContainer.innerHTML = message("error", error);
+    resultsContainer.innerHTML = error.message;
   }
 }
 
