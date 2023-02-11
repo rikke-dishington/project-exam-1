e8f0d12f7914bdb065491fff07e6fb39cf6f1c71
rikke-dishington <85433495+rikke-dishington@users.noreply.github.com>
2023-02-11 20:18:34 +0100

    Optimize css

--- js/data/dogs.js
+++ js/data/dogs.js
@@ -10,7 +10,7 @@ const dogA = {
 const dogB = {
   info: {
     image: "/images/pippin_field_trial.png",
-    name: "Pippin",
+    name: "Peregrin Tók",
     description:
       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
   },
@@ -19,7 +19,7 @@ const dogB = {
 const dogC = {
   info: {
     image: "/images/sage_goose.png",
-    name: "Sage",
+    name: "Workingmode Sage",
     description:
       "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
   },
@@ -28,7 +28,7 @@ const dogC = {
 const dogD = {
   info: {
     image: "/images/marco_water_apport.png",
-    name: "Marco",
+    name: "Fantomet Apporter",
     description:
       "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.",
   },
